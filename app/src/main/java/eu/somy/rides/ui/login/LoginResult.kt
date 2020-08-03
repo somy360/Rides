e567a1b91e1337527ac4088b1d13787601684512
Graeme Somerville <somy360@gmail.com>
2020-08-03 06:51:53 +0100

    Initial commit

---
+++ app/src/main/java/eu/somy/rides/ui/login/LoginResult.kt
@@ -0,0 +1,9 @@
+package eu.somy.rides.ui.login
+
+/**
+ * Authentication result : success (user details) or error message.
+ */
+data class LoginResult(
+        val success: LoggedInUserView? = null,
+        val error: Int? = null
+)
